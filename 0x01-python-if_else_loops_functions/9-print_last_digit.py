3fc3615bd9bce7de002fa31485e7f3b8c188b87b
Rcastro89 <r_castro_m@yahoo.es>
2021-12-21 22:59:08 -0500

    errors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
+    if number > -1:
+        lastdigit = number % 10
+    else:
+        lastdigit = ((number * -1) % 10) * -1
     lastdigit = int(repr(number)[-1])
-    print(lastdigit, end = "")
+    print(lastdigit, end="")
     return lastdigit
