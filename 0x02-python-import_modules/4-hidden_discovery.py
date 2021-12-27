620e23a64c8919ed3efe81fabd9413d6e3df01b0
Rcastro89 <r_castro_m@yahoo.es>
2021-12-27 14:28:14 -0500

    hidden.3

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import hidden_4
-    hidden = dir(hidden_4)
-    for search_ in hidden:
-        if search_[0] != '_':
-            print(search_)
+    names = dir(hidden_4)
+    for i in names:
+        if i[0] != '_' and i[1] != '_':
+            print(i)
