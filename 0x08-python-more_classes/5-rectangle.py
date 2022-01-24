9740bfba0ffcee443aefb18b871ffef8933bb3da
Rcastro89 <r_castro_m@yahoo.es>
2022-01-24 15:45:40 -0500

    4 del

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -64,3 +64,13 @@ class Rectangle():
             cad = cad + "\n"
         cad = cad[:-1]
         return (cad)
+
+    def __repr__(self):
+        """return str repr oficial"""
+        rep = ("Rectangle(" + str(self.__width) +
+               ", " + str(self.__height) + ")")
+        return (rep)
+
+    def __del__(self):
+        """delete object"""
+        print("Bye rectangle...")
