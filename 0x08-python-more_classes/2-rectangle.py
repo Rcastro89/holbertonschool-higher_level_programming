51e53e131024df8ea8a5f1320fa55daa4dabbd98
Rcastro89 <r_castro_m@yahoo.es>
2022-01-24 11:08:24 -0500

    2 area and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Module for print class empty"""
+
+
+class Rectangle():
+    """class empty"""
+
+    def __init__(self, width=0, height=0):
+        """generate atribute private
+        self: """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """method getter, get value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """method setter, set value of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """method getter, get value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """method setter, set value of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """calculate area of rectangle"""
+        area = self.__width * self.__height
+        return (area)
+
+    def perimeter(self):
+        """calculate perimeter of rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return (0)
+        per = (self.__width * 2) + (self.__height * 2)
+        return (per)
