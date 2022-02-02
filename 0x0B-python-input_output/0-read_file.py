82ef06202c0738a526db89030a13cda25f6e49c5
Rcastro89 <r_castro_m@yahoo.es>
2022-02-01 21:58:01 -0500

    0read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""read file"""
+
+
+def read_file(filename=""):
+    """read file again"""
+
+    with open("my_file_0.txt", "r", encoding="utf-8") as files:
+        for i in files:
+            print(i, end="")
+        print()
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,5 @@
+# Python - Input/Output
+## Files
+| Filename | Description |
+| :-------------- | :-----------: |
+| `0-read_file.py` | Write a function that reads a text file (UTF8) and prints it to stdout. |
