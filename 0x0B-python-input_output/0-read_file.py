59d665ebea366e3d341ee560125a8eae6661204a
Rcastro89 <r_castro_m@yahoo.es>
2022-02-01 22:00:19 -0500

    corrections

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 def read_file(filename=""):
     """read file again"""
 
-    with open("my_file_0.txt", encoding="utf-8") as files:
+    with open(filename, encoding="utf-8") as files:
         for i in files:
             print(i, end="")
         print()
