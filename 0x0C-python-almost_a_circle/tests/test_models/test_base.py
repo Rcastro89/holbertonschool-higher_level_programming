24d685dc338e801159805621a0ecfb4b1c6bc56d
Rcastro89 <r_castro_m@yahoo.es>
2022-02-07 22:24:28 -0500

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """test"""
 import unittest
-import pep8
 import os
 
 from models.base import Base
@@ -21,12 +20,6 @@ class TestBaseClass(unittest.TestCase):
         self.assertIsNotNone(Base.__doc__)
 
     # def test_methods_docstring(self):
-
-    def test_pep8_model(self):
-        """ pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        pep = style.check_files(["models/base.py"])
-        self.assertEqual(pep.total_errors, 0)
 
     def test_initialization_with_empty_id(self):
         """ ok"""
