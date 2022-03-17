3cafeab4b4bcc9a430980f659f0dcca7c3194941
Rcastro89 <r_castro_m@yahoo.es>
2022-03-17 03:37:30 -0500

    genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- Coutn genres
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
-LEFT OUTER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
