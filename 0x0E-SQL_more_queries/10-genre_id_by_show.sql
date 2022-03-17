a93a0ce52484df1eea360f82b2c6840641c1a9ba
Rcastro89 <r_castro_m@yahoo.es>
2022-03-17 02:49:11 -0500

    inner join double

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,4 @@
 -- dumps
-SELECT * FROM tv_genres;
-SELECT * FROM tv_show_genres;
-SELECT * FROM tv_shows;
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
