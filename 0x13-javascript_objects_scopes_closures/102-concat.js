d2363235d6338f9f40ad8d8928c3bbff77eacb1f
Rcastro89 <r_castro_m@yahoo.es>
2022-04-07 23:11:01 -0500

    files read and write

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,4 +3,4 @@
 const fs = require('fs');
 const arch1 = fs.readFileSync(process.argv[2], 'utf8');
 const arch2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.appendFileSync(process.argv[4], arch1 + '\n' + arch2 + '\n', 'utf8');
+fs.appendFileSync(process.argv[4], arch1 + arch2, 'utf8');
