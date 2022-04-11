8105f645561027b54b06d05c9b83c247c330afd6
Rcastro89 <r_castro_m@yahoo.es>
2022-04-11 17:37:03 -0500

    all pcharacters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+try {
+    const url = 'https://swapi-api.hbtn.io/api/films';
+    request(url, (error, response, body) => {
+        if (error) throw error;
+        const obj = JSON.parse(body);
+        cantPersonajes = (obj.results[process.argv[2] - 1].characters).length;
+        for (let x = 0; x < cantPersonajes; x++) {
+            const url1 = obj.results[process.argv[2] - 1].characters[x];
+            request(url1, (error1, response1, body1) => {
+                const obj1 = JSON.parse(body1);
+                console.log(obj1.name);
+            });
+        }
+
+    });
+} catch (error) {
+    console.log(error);
+}
