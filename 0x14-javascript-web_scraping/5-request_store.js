b25b8cb574cd1fd6ec2010dc84beb5edc44bc8a0
Rcastro89 <r_castro_m@yahoo.es>
2022-04-11 14:44:56 -0500

    file response body

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+try {
+
+    const url = process.argv[2];
+    request(url, (error, response, body) => {
+        if (error) throw error;
+        fs.writeFile(process.argv[3], response.body, 'utf-8', (err) => {
+            if (err) throw err;
+        });
+    });
+} catch (error) {
+    console.log(error);
+}
