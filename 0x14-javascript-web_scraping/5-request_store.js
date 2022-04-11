c2af038e6671d1991aa271092f28ec194a6d6d69
Rcastro89 <r_castro_m@yahoo.es>
2022-04-11 14:46:46 -0500

    file response body

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 const request = require('request');
 
 try {
-
-    const url = process.argv[2];
-    request(url, (error, response, body) => {
-        if (error) throw error;
-        fs.writeFile(process.argv[3], response.body, 'utf-8', (err) => {
-            if (err) throw err;
-        });
+  const url = process.argv[2];
+  request(url, (error, response, body) => {
+    if (error) throw error;
+    fs.writeFile(process.argv[3], response.body, 'utf-8', (err) => {
+      if (err) throw err;
     });
+  });
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
