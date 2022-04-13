552263c366d4d0cdd314f93c7bd24163d601061b
Rcastro89 <r_castro_m@yahoo.es>
2022-04-13 18:07:27 -0500

    update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,31 @@
 #!/usr/bin/node
 
 const request = require('request');
-const personajes = [];
-try {
-  const url = 'https://swapi-api.hbtn.io/api/films';
-  request(url, (error, response, body) => {
-    if (error) throw error;
-    const obj = JSON.parse(body);
-    const cantPersonajes = (obj.results[process.argv[2] - 1].characters).length;
-    for (let x = 0; x < cantPersonajes; x++) {
-      const url1 = obj.results[process.argv[2] - 1].characters[x];
-      const d = request(url1, (error1, response1, body1) => {
-        const obj1 = JSON.parse(body1);
-        personajes.push(obj1.name);
-        return obj1.name;
-      });
+let personajesOrdenados = [];
+let personajes = [];
+
+function buscar() {
+    try {
+        const url = 'https://swapi-api.hbtn.io/api/films';
+        request(url, (error, response, body) => {
+            if (error) throw error;
+            const obj = JSON.parse(body);
+            const cantPersonajes = (obj.results[process.argv[2] - 1].characters).length;
+            personajesOrdenados = Object.assign([], obj.results[process.argv[2] - 1].characters);
+            console.log(personajesOrdenados);
+            for (let x = 0; x < cantPersonajes; x++) {
+                const url1 = personajesOrdenados[x];
+                const d = request(url1, (error1, response1, body1) => {
+                    const obj1 = JSON.parse(body1);
+                    const nombre = obj1.name;
+                    personajes[url1] = nombre;
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
     }
-  });
-} catch (error) {
-  console.log(error);
 }
+
+buscar();
+console.log(personajes);
