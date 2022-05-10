610451ae4c20350d399b4ecba998a83f748d8d2c
Rcastro89 <r_castro_m@yahoo.es>
2022-05-10 18:17:21 -0500

    Key fore

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module for conect to database"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """generate main"""
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    var = "SELECT cities.id, cities.name, states.name FROM states\
+        INNER JOIN cities ON states.id = cities.state_id\
+        ORDER BY cities.id ASC"
+    cur.execute(var)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
