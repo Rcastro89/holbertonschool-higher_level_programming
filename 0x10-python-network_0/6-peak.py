0b6760546459d94bc20379a48a5c660c28f37739
Rcastro89 <r_castro_m@yahoo.es>
2022-05-22 23:57:53 -0500

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""modeulo to fidn peak"""
+
+def find_peak(list_of_integers):
+    """funtion to found peak"""
+    numer = None
+    if list_of_integers:
+        numer = max(list_of_integers)
+    return numer
