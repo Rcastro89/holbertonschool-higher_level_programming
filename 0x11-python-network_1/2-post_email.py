1099e4134b61bae7cdd338a69e70721c7ccc1ea3
Rcastro89 <r_castro_m@yahoo.es>
2022-05-23 23:33:59 -0500

    email POST

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Parametre POST
 """
-from urllib import request, parse
+import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     value = {'email': email}
-    data = parse.urlencode(value)
+    data = urllib.parse.urlencode(value)
     data = data.encode('utf-8')
-    with request.urlopen(url, data) as response:
+    with urllib.request.urlopen(url, data) as response:
         answer = response.read().decode('utf-8')
     print(answer)
