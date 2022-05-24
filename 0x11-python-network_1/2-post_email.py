4f1517351c894fa985e3813b6a67bebd10443e91
Rcastro89 <r_castro_m@yahoo.es>
2022-05-23 23:36:03 -0500

    email POST

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Parametre POST
+"""POST REQUEST
 """
 import urllib.request
 import urllib.parse
 import sys
 
 if __name__ == "__main__":
+
     url = sys.argv[1]
     email = sys.argv[2]
     value = {'email': email}
